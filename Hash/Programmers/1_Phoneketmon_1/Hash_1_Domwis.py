81729c76cd9efb74e1018056f965f868ec22d667
류지혜 <domwis@lyujihyes-MacBook-Air.local>
2023-09-10 12:11:27 +0900

    feat(Hash_1_Domwis.py): Write answer code for Hash 1 problem

--- Hash/Programmers/1_Phoneketmon_1/Hash_1_Domwis.py
+++ Hash/Programmers/1_Phoneketmon_1/Hash_1_Domwis.py
@@ -1,1 +1,16 @@
 # Quiz: https://school.programmers.co.kr/learn/courses/30/lessons/1845
+def solution(nums):
+    count = len(nums) // 2
+    unique_values = {}
+    for n in nums:
+        if n not in unique_values.keys():
+            unique_values[n] = 1
+        else:
+            unique_values[n] += 1
+    if count <= len(unique_values.keys()):
+        return count
+    else:
+        return len(unique_values.keys())
+
+def __main__():
+    print(solution([3,1,2,3]))
