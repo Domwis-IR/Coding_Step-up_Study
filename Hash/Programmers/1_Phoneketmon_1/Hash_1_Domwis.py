ffec704a9abfc43571a6438c21eec1959f01f3c6
류지혜 <domwis@lyujihyes-MacBook-Air.local>
2023-09-17 13:44:29 +0900

    Upload All Solution for Hash Problem

--- Hash/Programmers/1_Phoneketmon_1/Hash_1_Domwis.py
+++ Hash/Programmers/1_Phoneketmon_1/Hash_1_Domwis.py
@@ -1,4 +1,5 @@
 # Quiz: https://school.programmers.co.kr/learn/courses/30/lessons/1845
+
 def solution(nums):
     count = len(nums) // 2
     unique_values = {}
@@ -12,5 +13,3 @@ def solution(nums):
     else:
         return len(unique_values.keys())
 
-def __main__():
-    print(solution([3,1,2,3]))
